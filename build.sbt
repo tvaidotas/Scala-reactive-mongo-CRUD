c3dc3786a76a5ba9275e4f294c25e95b40356b19
admin <tadas.vaidotas@gmail.com>
2018-12-11 21:37:51 +0000

    Added constants file Implemented insert one and many documents Implemented remove one and many documents

--- build.sbt
+++ build.sbt
@@ -5,5 +5,6 @@ version := "0.1"
 scalaVersion := "2.12.8"
 
 libraryDependencies ++= Seq(
-  "org.reactivemongo" %% "reactivemongo" % "0.16.0"
+  "org.reactivemongo" %% "reactivemongo" % "0.16.0",
+  "org.slf4j" % "slf4j-log4j12" % "1.7.25"
 )
--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -1,27 +1,6 @@
-import reactivemongo.api.MongoDriver
+import reactivemongo.bson.BSONDocument
+import service.DBConnection
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-import scala.concurrent.Await
-
-object Runner {
-
-  val uri = "mongodb://localhost"
-
-  def main(args: Array[String]): Unit = {
-    // Create Mongo Driver
-    val driver = new MongoDriver
-
-    // Connect to localhost
-    val connection = driver.connection(List("localhost"))
-
-    // Connect to mongodb
-    //val db = Await.result(connection.database("reactivemongo"), Duration(5000, "millis"))
-    val db = Await.result(connection.database("reactivemongo"), Duration(5000, "millis"))
-
-    // Connect to mongodb collection
-    val collection = db.collection("persons")
-  }
-
+object Runner extends App with DBConnection {
 
 }
