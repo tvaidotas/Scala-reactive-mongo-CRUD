b883fc4b893ccef42c62a75f070148f27d2d5aa5
tvaidotas <tadas.vaidotas@gmail.com>
2020-03-10 15:56:20 +0000

    small refactoring

--- src/main/scala/models/Person.scala
+++ src/main/scala/models/Person.scala
@@ -3,7 +3,6 @@ package models
 import org.mongodb.scala.bson.ObjectId
 
 object Person {
-  def apply(firstName: String, lastName: String): Person =
-    Person(new ObjectId(), firstName, lastName)
+  def apply(firstName: String, lastName: String): Person = Person(new ObjectId(), firstName, lastName)
 }
 case class Person(_id: ObjectId, firstName: String, lastName: String)
